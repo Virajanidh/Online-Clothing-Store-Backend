8bc4d3f3133f9dbf1abce59f11f3e6556dfe73cc
Virajanidh <59724561+Virajanidh@users.noreply.github.com>
2021-10-29 03:22:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.listen(port, () => console.log('Server is running on port' + port))
 //app.use((req, res, next) => {
 //    next(createError(404));
 //});
-app.use("*",(req,res)=>res.status(404).json({error:"Error found"}))
+app.use("*",(req,res)=>res.status(404).json({error:"Error found !!"}))
 
 app.use(function (err, req, res, next) {
     console.error(err.message);
